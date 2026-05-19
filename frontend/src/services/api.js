fb7c99e5555ba4568c4316a630fc8b22e3175a83
Nitika Maan <nitikamaan15209@gmail.com>
2026-05-19 05:42:05 +0530

    api update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,14 +4,14 @@ const API = "https://syncbasebackend.onrender.com";
 
 // Employee APIs
 export const addEmployee = (data) =>
-  axios.post(`${API}/employees`, data);
+  axios.post(`${API}/api/employees`, data);
 
 export const getEmployees = () =>
-  axios.get(`${API}/employees`);
+  axios.get(`${API}/api/employees`);
 
 export const deleteEmployee = (id) =>
-  axios.delete(`${API}/employees/${id}`);
+  axios.delete(`${API}/api/employees/${id}`);
 
 // AI API
 export const getAIRecommendation = (data) =>
-  axios.post(`${API}/ai/recommendation`, data);
+  axios.post(`${API}/api/ai/recommendation`, data);
